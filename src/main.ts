f5df5064cf6504c3379db31a21db44f08069c85a
flowing-abyss <flowing.abyss@gmail.com>
2026-03-21 01:46:16 +0700

    feat: implement HybridSearchPlugin main entry

--- src/main.ts
+++ src/main.ts
@@ -1,99 +1,63 @@
-import {App, Editor, MarkdownView, Modal, Notice, Plugin} from 'obsidian';
-import {DEFAULT_SETTINGS, MyPluginSettings, SampleSettingTab} from "./settings";
+import { Notice, Plugin } from 'obsidian';
+import { SearchClient } from './ipc';
+import type { HybridSearchSettings } from './settings';
+import { DEFAULT_SETTINGS, HybridSearchSettingTab } from './settings';
+import { SearchModal } from './ui/SearchModal';
 
-// Remember to rename these classes and interfaces!
+export default class HybridSearchPlugin extends Plugin {
+  settings!: HybridSearchSettings;
+  client?: SearchClient;
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+  async onload(): Promise<void> {
+    await this.loadSettings();
 
-	async onload() {
-		await this.loadSettings();
+    const bin = this.settings.binaryPath || 'obsidian-hybrid-search';
+    const vault = (this.app.vault.adapter as { getBasePath?: () => string }).getBasePath?.() ?? '';
+    this.client = new SearchClient(bin, vault);
 
-		// This creates an icon in the left ribbon.
-		this.addRibbonIcon('dice', 'Sample', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
+    this.client.waitReady().catch(() => {
+      new Notice(
+        'Hybrid search: server did not start. Check that Obsidian-hybrid-search is installed.',
+        8000,
+      );
+    });
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status bar text');
+    this.addCommand({
+      id: 'open-search',
+      name: 'Open search',
+      callback: () => {
+        if (!this.client) {
+          new Notice('Hybrid search: client not ready.');
+          return;
+        }
+        new SearchModal(this.app, this.client, this.settings).open();
+      },
+    });
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-modal-simple',
-			name: 'Open modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'replace-selected',
-			name: 'Replace selected content',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				editor.replaceSelection('Sample editor command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-modal-complex',
-			name: 'Open modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+    this.addRibbonIcon('search', 'Hybrid search', () => {
+      if (!this.client) {
+        new Notice('Hybrid search: client not ready.');
+        return;
+      }
+      new SearchModal(this.app, this.client, this.settings).open();
+    });
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-				return false;
-			}
-		});
+    this.addSettingTab(new HybridSearchSettingTab(this.app, this));
+  }
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+  onunload(): void {
+    this.client?.dispose();
+  }
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			new Notice("Click");
-		});
+  async loadSettings(): Promise<void> {
+    this.settings = Object.assign(
+      {},
+      DEFAULT_SETTINGS,
+      (await this.loadData()) as Partial<HybridSearchSettings>,
+    );
+  }
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-
-	}
-
-	onunload() {
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData() as Partial<MyPluginSettings>);
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		let {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
+  async saveSettings(): Promise<void> {
+    await this.saveData(this.settings);
+  }
 }
